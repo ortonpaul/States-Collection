cc714d3b6ab1f6af956e14f72165aa46f9b1cb31
Paul Orton <ortonpaul18@gmail.com>
2019-02-28 14:36:17 -0600

    Created dictionary

--- States Collection/main.swift
+++ States Collection/main.swift
@@ -6,4 +6,24 @@ import Foundation
 
 var states = [String: String]()
 
+states["MO"] = "Missouri"
+states["PA"] = "Pennsylvania"
+states["CA"] = "California"
 
+for (code, name) in states {
+    print("\(code) is \(name)")
+}
+
+let stateCodes = [String] (states.keys)
+
+for code in stateCodes {
+    print(code)
+}
+
+states["PA"] = nil
+
+states.removeValue(forKey: "MO")
+
+for (code, name) in states {
+    print("\(code) is \(name)")
+}
